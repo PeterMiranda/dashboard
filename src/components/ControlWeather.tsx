0d84ea951f64bf4b7ac247a396491f4c90456478
PeterMiranda <96308033+PeterMiranda@users.noreply.github.com>
2024-12-17 01:03:39 -0500

    Guía 13

--- src/components/ControlWeather.tsx
+++ src/components/ControlWeather.tsx
@@ -1,16 +1,29 @@
 //GUÍA 12
 
-{/* Componentes MUI */}
+    //GUÍA 13
+    {/* Hooks */ }
+    import { useState, useRef } from 'react';
 
+{/* Componentes MUI */}
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+//import Select from '@mui/material/Select';
+
+    //GUÍA 13
+    {/* Interfaz SelectChangeEvent */}
+    import Select, { SelectChangeEvent } from '@mui/material/Select';
    
 export default function ControlWeather() {
+
+    {/* Constante de referencia a un elemento HTML */ }
+    const descriptionRef = useRef<HTMLDivElement>(null);
+
+    {/* Variable de estado y función de actualización */}
+    let [selected, setSelected] = useState(-1)
 
     {/* Arreglo de objetos */}
     let items = [
@@ -21,6 +34,20 @@ export default function ControlWeather() {
 
     {/* Arreglo de elementos JSX */}
     let options = items.map( (item, key) => <MenuItem key={key} value={key}>{item["name"]}</MenuItem> )
+
+    {/* Manejador de eventos */}
+    const handleChange = (event: SelectChangeEvent) => {
+			
+        let idx = parseInt(event.target.value)
+        // alert( idx );
+        setSelected( idx );
+
+        {/* Modificación de la referencia descriptionRef */}
+        if (descriptionRef.current !== null) {
+            descriptionRef.current.innerHTML = (idx >= 0) ? items[idx]["description"] : ""
+        }
+   
+    };
        
     {/* JSX */}
     return (
@@ -45,6 +72,7 @@ export default function ControlWeather() {
                         id="simple-select"
                         label="Variables"
                         defaultValue='-1'
+                        onChange={handleChange}
                     >
                         <MenuItem key="-1" value="-1" disabled>Seleccione una variable</MenuItem>
 
@@ -55,9 +83,16 @@ export default function ControlWeather() {
 
             </Box>
 
+            {/* Use la variable de estado para renderizar del item seleccionado */}
+            {/*<Typography mt={2} component="p" color="text.secondary">
+            {
+                (selected >= 0)?items[selected]["description"]:""
+            }
+            </Typography>*/}
+
+            <Typography ref={descriptionRef} mt={2} component="p" color="text.secondary" />
 
         </Paper>
-
 
     )
 }
